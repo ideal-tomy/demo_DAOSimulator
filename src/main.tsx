462ecaa9489567e40e7dc0976bf0e75553d7dd27
ideal-tomy <ryojitomii@gmail.com>
2025-11-11 05:45:15 +0900

    デプロイ調整

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <DemoProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/demo_DAOSimulator">
         <App />
       </BrowserRouter>
     </DemoProvider>
